89ee607950e99ccf2ce7f6967a2620cb3b56c2b1
FlyingChilli04 <lmaisnam2@gmail.com>
2024-01-24 20:33:07 +1100

    Fixed booking page

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,10 +1,9 @@
 "use client"
-import { InlineWidget } from "react-calendly";
 
 export default function Contact() {
     return (
         <div>
-            <InlineWidget url="https://calendly.com/khuynh8" />
+            contact!
         </div>
     )
 }
