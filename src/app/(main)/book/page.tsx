0dc4959619b239560b7d2dbcdc9b08ed0145c3b1
FlyingChilli04 <143712553+FlyingChilli04@users.noreply.github.com>
2024-01-25 22:01:45 +1100

    Creating contact page (#4)
    * Page: Portfolio page created, calls strapi to fetch images. Fix: Footer stays at the bottom now;
    
    * Added toast error and removed old images
    
    * Fixed booking page
    
    * Finished contact page
    
    ---------
    
    Co-authored-by: Kieren <kierenhuynh88@gmail.com>

---
+++ src/app/(main)/book/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { InlineWidget } from "react-calendly";
+
+export default function Contact() {
+    return (
+        <div>
+            <InlineWidget url="https://calendly.com/khuynh8" />
+        </div>
+    )
+}
