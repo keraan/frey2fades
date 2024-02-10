fb53bcda20f4ec9810308effbe3d1296fb68b803
FlyingChilli04 <lmaisnam2@gmail.com>
2024-02-10 22:42:54 +1100

    Linted home page code

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Image, Link, Button } from "@nextui-org/react";
+import { Link, Button } from "@nextui-org/react";
 import StickyCursor from "@/app/components/StickyCursor";
 import { useRef } from "react";
 import BookingButton from "@/app/components/BookingButton";
@@ -9,19 +9,25 @@ export default function Home() {
   return (
     <div className="w-full">
       <StickyCursor stickyElement={stickyElement} />
-        <div className="flex flex-col justify-center items-center m-1 h-full">
-          <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-black opacity-70">Frey2Fades</h1>
-          <div className="flex flex-row my-5">
-            <p className="text-black opacity-70">
-              A Blacktown based self taught barber
-            </p>
-          </div>
-          <Link href="/booking" className="text-black mb-5">
-            <Button radius="full" className="bg-gradient-to-tr from-black-500 to-white-500 shadow-lg" color="primary">
-              <BookingButton ref={stickyElement} />
-            </Button>
-          </Link>
+      <div className="flex flex-col justify-center items-center m-1 h-full">
+        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-black opacity-70">
+          Frey2Fades
+        </h1>
+        <div className="flex flex-row my-5">
+          <p className="text-black opacity-70">
+            A Blacktown based self taught barber
+          </p>
         </div>
+        <Link href="/booking" className="text-black mb-5">
+          <Button
+            radius="full"
+            className="bg-gradient-to-tr from-black-500 to-white-500 shadow-lg"
+            color="primary"
+          >
+            <BookingButton ref={stickyElement} />
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
